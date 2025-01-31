efc2055312be74fe09ba32f5b7a5f613f141a1f7
Robert Harrison <harrisonrw@gmail.com>
2025-01-31 14:25:30 -0800

    Initial Xcode Project

---
+++ HideNavBarOnScroll/HideNavBarOnScroll/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HideNavBarOnScroll
+//
+//  Created by Robert Harrison on 1/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HideNavBarOnScroll/HideNavBarOnScroll/HideNavBarOnScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  HideNavBarOnScrollApp.swift
+//  HideNavBarOnScroll
+//
+//  Created by Robert Harrison on 1/31/25.
+//
+
+import SwiftUI
+
+@main
+struct HideNavBarOnScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
